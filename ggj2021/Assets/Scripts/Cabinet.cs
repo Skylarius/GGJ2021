93251582fc78dde0970f48f97d777d1c5f6793b4
Jacopo Fabrini <JacopoFabrini@users.noreply.github.com>
2021-01-30 16:05:33 +0100

    Added GameManager, Fragments, Memory, Dialogue scripts

---
+++ ggj2021/Assets/Scripts/Cabinet.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cabinet : MonoBehaviour
+{
+    List<Memory> Memories = new List<Memory>();
+    public GameObject interactionButton;
+    bool isPlayerNearby = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactionButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPlayerNearby)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                DepositAllFragments();
+            }
+        }
+    }
+
+    //Detect if player is nearby.
+    //If true, it shows prompt to deposit fragments and enable deposit mech.
+    //If enemy is nearby it remove one fragment from one memory.
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collision Happened!");
+        if(col.gameObject.tag == "Player" && GameManager.inventory.Count > 0)
+        {
+            interactionButton.SetActive(true);
+            isPlayerNearby = true;
+        }
+        else if(col.gameObject.tag == "thief")
+        {
+            //StealFragment();
+        }
+    }
+
+    //Remove prompt to deposit fragments and disable deposit mech.
+    void OnCollisionExit(Collision col)
+    {
+        Debug.Log("Exited Collision!");
+        if(col.gameObject.tag == "Player")
+        {
+            interactionButton.SetActive(false);
+            isPlayerNearby = false;
+        }
+    }
+
+    void DepositAllFragments()
+    {
+        int inventoryCount = GameManager.inventory.Count;
+
+        for(int i = 0; i < inventoryCount; i++)
+        {
+            GameObject fragment = GameManager.inventory.Dequeue();
+            GameObject memory = fragment.GetComponent<Fragment>().memory;
+            memory.GetComponent<Memory>().AddFragment(fragment);
+        }
+    }
+
+    /*public Fragment StealFragment()
+    {
+
+    }*/
+}
---
+++ ggj2021/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static Queue<GameObject> inventory = new Queue<GameObject>(3);
+
+    void AddFragmentToInventory(GameObject f)
+    {
+        inventory.Enqueue(f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
