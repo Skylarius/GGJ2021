6c5f3d061ce4b9f26ba1b33e9bdfc938e0b18b78
Jacopo Fabrini <JacopoFabrini@users.noreply.github.com>
2021-01-31 13:14:22 +0100

    Fixed menus (issued reproducing button sound, NOT BLOCKING)

--- ggj2021/Assets/Scripts/MainMenu.cs
+++ ggj2021/Assets/Scripts/MainMenu.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
-{  
+{
+    public AudioSource clickSound;
+    public AudioClip playSound;
+    public AudioClip quitSound;
+
     public void PlayGame()
     {
+        clickSound.clip = playSound;
+        clickSound.Play();
+
         SceneManager.LoadScene("MainScene");
     }
 
     public void QuitGame()
     {
+        clickSound.clip = quitSound;
+        clickSound.Play();
+
         Application.Quit();
     }
 }
