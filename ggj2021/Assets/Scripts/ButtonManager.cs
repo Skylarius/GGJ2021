1e46a6d68c76e1c445b2788c797d5cf49714b950
Ilario Gerloni <ilario.gerloni@gmail.com>
2021-02-20 01:03:36 +0100

    add joypad buttons if joypad in

---
+++ ggj2021/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Sprite KeyboardSprite;
+    public Sprite XBoxSprite;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        if (Input.GetJoystickNames().Length > 0 && Input.GetJoystickNames()[0].ToLower().Contains("xbox")) {
+            GetComponent<SpriteRenderer>().sprite = XBoxSprite;
+        } else {
+            GetComponent<SpriteRenderer>().sprite = KeyboardSprite;
+        }
+    }
+
+}
