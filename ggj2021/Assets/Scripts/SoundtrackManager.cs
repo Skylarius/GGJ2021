776319c05c43c0b2043a86e17ba8dbeaa8cf5f6a
Jacopo Fabrini <JacopoFabrini@users.noreply.github.com>
2021-03-14 17:03:22 +0100

    WIP Implementation of incremental soundtrack
    The code for the incremental soundtrack has been put in place and tested.
    Before merging this I need to:
    
    -)Have the new 9 tracks of the incremental soundtrack.
    -)Assign them to the list of tracks.
    
    We start at 0 memory with no soundtrack at all (I'd like the player to understand it is incrementing while depositing memories).
    We do not need track 10 since at ten the music stops and the piano appears.

---
+++ ggj2021/Assets/Scripts/SoundtrackManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundtrackManager : MonoBehaviour
+{
+    public List<AudioClip> soundtracks = new List<AudioClip>(9);
+
+}
